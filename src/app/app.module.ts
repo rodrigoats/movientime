63229c6cb9e3861476edc327922b46ea4e419331
Rodrigo Targino <rodrigoats1@gmail.com>
2021-06-17 22:50:30 +0100

    Implement oauth with google login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,22 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireModule } from "@angular/fire";
+import { GooglePlus } from "@ionic-native/google-plus/ngx";
+import { environment } from "src/environments/environment"
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule],
+  providers: [GooglePlus, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
